5cb736643f84d9bfd7b1fbdfc4515d27a06e0b59
Israel Adetunji <adisen230@gmail.com>
2020-11-04 11:48:32 +0100

    Added auth middleware

---
+++ src/middleware/auth.ts
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken'
+import { Request, Response, NextFunction } from 'express'
+
+module.exports = function (req: Request, res: Response, next: NextFunction) {
+  // Get token from header
+  const token = req.header('x-auth-token')
+
+  // Check if not token
+  if (!token) {
+    return res.status(401).json({ msg: 'No token authorization denied' })
+  }
+
+  // Verif token
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!)
+    req.user = decoded.user
+    next()
+  } catch (error) {
+    res.status(401).json({ msg: 'Token is not valid' })
+  }
+}
